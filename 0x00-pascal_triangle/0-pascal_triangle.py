8d65eb53916b234847bcf81b558d6ffda90e5d8c
Dada Olajide <olajidedada74@gmail.com>
2023-07-30 23:54:30 +0100

    Pascal Traingle Task

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,28 +1,23 @@
+#!/usr/bin/python3
+""" This module documents the python implementation of the pascals triangle """
+
+
 def pascal_triangle(n):
+    """ A Pascal's triangle function by list comprehension expansion """
+
+    triangle = []
     if n <= 0:
         return []
 
-    triangle = [[1]]  # Initialize the triangle with the first row [1]
+    start = [1]
+    triangle.append(start)
+    if n == 1:
+        return triangle
 
-    # Generate the remaining rows
-    for i in range(1, n):
-        prev_row = triangle[-1]  # Get the previous row
-        new_row = [1]  # Start the new row with 1
-
-        # Calculate the values in between using the sum of two consecutive elements from the previous row
-        for j in range(1, i):
-            new_row.append(prev_row[j - 1] + prev_row[j])
-
-        new_row.append(1)  # End the new row with 1
-        triangle.append(new_row)  # Add the new row to the triangle
+    for i in range(n - 1):
+        start = [start[i] + start[i + 1] for i in range(0, len(start) - 1)]
+        start.insert(0, 1)
+        start.append(1)
+        triangle.append(start)
 
     return triangle
-
-# Test the function
-if __name__ == "__main__":
-    def print_triangle(triangle):
-        for row in triangle:
-            print("[{}]".format(",".join([str(x) for x in row])))
-
-    triangle_5 = pascal_triangle(5)
-    print_triangle(triangle_5)
